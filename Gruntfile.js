9fcaa05b4e2a3aa665c430ee9f457899392f7c5a
MauricioFGF <mfgf@cin.ufpe.br>
2023-06-03 12:39:05 -0300

    Create Architecture

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,58 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
-	  pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
- 	  concat: {
- 		 options: {
-    			separator: ';'
-  		 },
-  	  	 dist: {
-			 files: {
-    			'build/strands/strands.json': ['app/strands/strands.json'],
-    			'build/popup.html': ['app/popup.html'],
-			}
-  		 }
- 	 },
+    concat: {
+      options: {
+        separator: ";",
+      },
+      dist: {
+        files: {
+          "build/strands/strands.json": ["app/strands/strands.json"],
+          "build/view/popup.html": ["app/view/popup.html"],
+        },
+      },
+    },
 
-	 uglify: {
-	 	my_target: {
-			files: {
-				'build/js/script.min.js': ['app/js/*.js']
-			}
-		}
-	 },
+    uglify: {
+      my_target: {
+        files: {
+          "build/js/script.min.js": [
+            "app/js/*.js",
+            "app/js/*/*.js",
+            "app/lib/ngDialog/*.js",
+          ],
+        },
+      },
+    },
 
-	 cssmin: {
-	 	options: {
-			shorthandCompacting: false,
-			roundingPrecision: -1
-		},
-		target: {
-			files: {
-				'build/css/style.min.css': ['app/css/*.css']
-			}
-		}
-	 },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1,
+      },
+      target: {
+        files: {
+          "build/css/style.min.css": [
+            "app/css/*.css",
+            "app/lib/ngDialog/*.css",
+          ],
+        },
+      },
+    },
 
-	 processhtml: {
-	 	options: {
-		},
-	 	dist: {
-			files: {
-				'build/index.html': ['app/index.html']
-			}
-		}
-	}
-  })
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-processhtml');
-grunt.registerTask('build', ['concat','uglify', 'cssmin', 'processhtml']);
+    processhtml: {
+      options: {},
+      dist: {
+        files: {
+          "build/index.html": ["app/index.html"],
+        },
+      },
+    },
+  });
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-processhtml");
+  grunt.registerTask("build", ["concat", "uglify", "cssmin", "processhtml"]);
 };
-
-
-
---
+++ app/js/app.js
@@ -0,0 +1,13 @@
+var MinIONApp = angular.module("MinIONApp", [
+  "dataSupplierService",
+  "sequenceMatcherService",
+  "sequenceDialogService",
+  "colorService",
+  "backendService",
+  "ngResource",
+  "dataCollectionService",
+  "dialogService",
+  "MinIONAppFilters",
+]);
+
+var MinIONAppFilters = angular.module("MinIONAppFilters", []);
